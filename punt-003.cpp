baf784bce1f614a262debdd587be41c3e2ba9dc3
BillyGarayUTN <bgarayquispe@frba.utn.edu.ar>
2023-07-07 01:31:06 -0300

    agrege hasta punt 3

---
+++ punt-003.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+/*
+- Pedir al humano la cantidad de numeros a ingresar y validar que sea
+mayor o igual a 1.
+- Pedir al humano que ingrese cada uno de los elementos.
+- Buscar el numero más pequeño y mostrarlo
+*/
+
+int main(){
+	
+	
+	
+	
+	
+	
+	return 0;
+}
